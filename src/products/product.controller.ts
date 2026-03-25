0adc679827a6771d39c70cb328f53df41a6fe73a
68011068 <mr.sirirsart1999@gmail.com>
2026-03-25 11:08:38 +0700

    6

--- src/products/product.controller.ts
+++ src/products/product.controller.ts
@@ -2,7 +2,7 @@ import { Controller, Get } from '@nestjs/common';
 import { ProductService } from './product.service';
 import type { ApiResponse } from 'src/interfaces/response.interface';
 import { Product } from './product.interface';
-@Controller('users')
+@Controller('products')
 export class ProductController {
   constructor(private readonly ProductService: ProductService) {}
   @Get()
